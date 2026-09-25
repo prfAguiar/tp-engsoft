230fafb4d93a230777d46b7fd34cb77472a9122e
Mateus <matsuramateus@gmail.com>
2026-09-25 00:50:56 -0300

    feat(users): configure authentication endpoints

--- backend/apps/users/urls.py
+++ backend/apps/users/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-
-from .views import InvestorProfileEvaluationView, InvestorProfileQuizQuestionsView
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from .views import InvestorProfileEvaluationView, InvestorProfileQuizQuestionsView, RegisterView
 
 urlpatterns = [
+    path('register/', RegisterView.as_view(), name='register'),
+    path('login/', TokenObtainPairView.as_view(), name='login'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('profile/quiz/questions/', InvestorProfileQuizQuestionsView.as_view(), name='investor-quiz-questions'),
     path('profile/quiz/evaluate/', InvestorProfileEvaluationView.as_view(), name='investor-quiz-evaluate'),
 ]
