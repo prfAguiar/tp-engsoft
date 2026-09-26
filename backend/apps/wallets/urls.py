f20504c7a18168fb41f93726c0f53dc306f5da9e
Renato Vilela <renatovmpacheco@gmail.com>
2026-09-25 21:33:03 -0300

    feat(wallets): add endpoints for creating and deleting wallet items

--- backend/apps/wallets/urls.py
+++ backend/apps/wallets/urls.py
@@ -1,6 +1,8 @@
-from django.urls import path
-from .views import WalletRetrieveUpdateView
+﻿from django.urls import path
+from .views import WalletRetrieveUpdateView, WalletItemCreateView, WalletItemDestroyView
 
 urlpatterns = [
     path('', WalletRetrieveUpdateView.as_view(), name='wallet-detail'),
+    path('items/', WalletItemCreateView.as_view(), name='wallet-item-create'),
+    path('items/<int:pk>/', WalletItemDestroyView.as_view(), name='wallet-item-delete'),
 ]
--- backend/apps/wallets/views.py
+++ backend/apps/wallets/views.py
@@ -1,14 +1,27 @@
-from rest_framework import generics
+﻿from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated
-from .models import Wallet
-from .serializers import WalletSerializer
+from .models import Wallet, WalletItem
+from .serializers import WalletSerializer, WalletItemSerializer
 
 class WalletRetrieveUpdateView(generics.RetrieveUpdateAPIView):
     permission_classes = (IsAuthenticated,)
     serializer_class = WalletSerializer
 
     def get_object(self):
-        # Retorna a carteira do usuário logado.
-        # Devido ao signal, ela sempre existirá.
         wallet, _ = Wallet.objects.get_or_create(user=self.request.user)
         return wallet
+
+class WalletItemCreateView(generics.CreateAPIView):
+    permission_classes = (IsAuthenticated,)
+    serializer_class = WalletItemSerializer
+
+    def perform_create(self, serializer):
+        wallet, _ = Wallet.objects.get_or_create(user=self.request.user)
+        serializer.save(wallet=wallet)
+
+class WalletItemDestroyView(generics.DestroyAPIView):
+    permission_classes = (IsAuthenticated,)
+    serializer_class = WalletItemSerializer
+
+    def get_queryset(self):
+        return WalletItem.objects.filter(wallet__user=self.request.user)
